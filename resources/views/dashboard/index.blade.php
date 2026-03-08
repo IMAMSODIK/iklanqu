695a32227ec85ad5d96bdd0a4c68dce134b0d994
IMAM SODIK <imamsodik00@gmail.com>
2026-03-09 05:58:41 +0700

    adf

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -579,7 +579,7 @@
     <div class="app-container">
         <!-- Status bar -->
         <div class="status-bar">
-            <img src="{{ asset('landing_assets/images/logo/logo.png') }}" width="15%" alt="logo" style="border-radius: 14px">
+            <img src="{{ asset('landing_assets/images/logo/logo.png') }}" width="20%" alt="logo" style="border-radius: 14px">
         </div>
 
         <!-- Area konten -->
