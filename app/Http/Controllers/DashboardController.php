7293b84da09a80dc6ea7e565db77cd969bcd4719
IMAM SODIK <imamsodik00@gmail.com>
2026-03-09 15:25:58 +0700

    asdf

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function index()
     {
         try {
+            $user = Auth::user()->role;
+            dd($user);
             $data = [
                 'pageTitle' => 'Dashboard'
             ];
