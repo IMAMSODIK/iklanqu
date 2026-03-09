33c63e51f3d16931e16e9b0ef0a95c53d9832a91
IMAM SODIK <imamsodik00@gmail.com>
2026-03-09 15:28:08 +0700

    asdf

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,13 +10,15 @@ class DashboardController extends Controller
     public function index()
     {
         try {
-            $user = Auth::user()->role;
-            dd($user);
             $data = [
                 'pageTitle' => 'Dashboard'
             ];
 
-            return view('dashboard.index', $data);
+            if(in_array(Auth::user()->role, ['admin', 'verifikator'])){
+                return view('dashboard.index_admin', $data);
+            }else{
+                return view('dashboard.index', $data);
+            }
         } catch (\Exception $e) {
             return back()->with('error', 'Terjadi kesalahan saat membuka halaman dashboard.');
         }
