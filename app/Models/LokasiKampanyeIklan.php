8f49c2a28c43f89df4dc5e4f52a48c9b450eb423
IMAM SODIK <imamsodik00@gmail.com>
2026-05-15 01:37:09 +0700

    adf

--- app/Models/LokasiKampanyeIklan.php
+++ app/Models/LokasiKampanyeIklan.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class LokasiKampanyeIklan extends Model
 {
@@ -11,4 +12,9 @@ class LokasiKampanyeIklan extends Model
     use HasFactory;
 
     protected $guarded = ['id'];
+
+    public function lokasi(): BelongsTo
+    {
+        return $this->belongsTo(Lokasi::class);
+    }
 }
